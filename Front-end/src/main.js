09fdc078b3f2baa3602b22bf1861e7cfb11379c1
Denis <denisromashov95@gmail.com>
2018-04-03 19:04:38 +0300

    init vue bootstrap webpack vuesax
    creation of the framework of the project

---
+++ Front-end/src/main.js
@@ -0,0 +1,27 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import Vuesax from 'vuesax'
+
+//Если использовать компоненты BootstrapVue включить!
+//import BootstrapVue from 'bootstrap-vue'
+//import 'bootstrap/dist/css/bootstrap.css'
+//import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'vuesax/dist/vuesax.css' //vuesax styles
+
+
+//Если использовать компоненты BootstrapVue включить!
+//Vue.use(BootstrapVue);
+Vue.use(Vuesax)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
